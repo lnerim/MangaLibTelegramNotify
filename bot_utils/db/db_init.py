b83ad887ab7b29a0dc462c82bd00a931f65b3317
lnerim <joing123456789@gmail.com>
2024-12-03 01:28:48 +0300

    Редизайн БД и прототипы интерфейса к ней

--- bot_utils/db/db_init.py
+++ bot_utils/db/db_init.py
@@ -1,32 +1,58 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Boolean
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+# TODO Надо идентификатор произведения, потом идентификатор сайта
+#  (так как на анимелиб другие идентификаторы, хотя на мангалибе и ранобэлибе они вроде одинаковые).
+#  Пусть они будут ключами. Далее имя пусть тоже будет в медии, так как это логично,
+#  а ключи пусть будут и обновлениях, чтобы точно обращаться к нужному. Причём в обновлениях должны
+#  быть уникальными пользователь и эти ключи из другого столбца.
+#  Остаётся вопрос, как удалять обновления, хотя может просто поставить флаг. Media я бы вообще не удалял
+
+# TODO: Удалить потом
+# https://github.com/kvesteri/sqlalchemy-utils/blob/baf53c/sqlalchemy_utils/models.py#L47
+def _my_repr(self):
+    state = sqlalchemy.inspect(self)
+    field_reprs = []
+    fields = state.mapper.columns.keys()
+    for key in fields:
+        value = state.attrs[key].loaded_value
+        if key in state.unloaded:
+            value = "N/A"
+        else:
+            value = repr(value)
+        field_reprs.append('='.join((key, value)))
+
+    return '{}({})'.format(self.__class__.__name__, ', '.join(field_reprs))
+
+Base.__repr__ = _my_repr
 
 
 class DBMedia(Base):
     __tablename__ = "media"
 
     media_id = Column(Integer, primary_key=True)  # ID произведения
+    site_id = Column(Integer, primary_key=True)  # ID сайта
 
-    media_type = Column(String, nullable=False)
+    media_name = Column(String, nullable=False)
 
     # Для манги и ранобэ
-    last_read_chapter = Column(Float, nullable=True)
-    last_read_volume = Column(Float, nullable=True)
+    last_read_chapter = Column(Float, nullable=False, default=0.0)
+    last_read_volume = Column(Float, nullable=False, default=0.0)
 
     # Для аниме
-    last_read_season = Column(Float, nullable=True)
-    last_read_episode = Column(Float, nullable=True)
+    last_read_season = Column(Float, nullable=False, default=0.0)
+    last_read_episode = Column(Float, nullable=False, default=0.0)
 
 
 class DBUpdates(Base):
     __tablename__ = "updates"
 
-    update_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)  # ID пользователя telegram
 
-    user_id = Column(Integer, nullable=False)  # ID пользователя telegram
+    media_id = Column(Integer, ForeignKey(DBMedia.media_id), primary_key=True)  # ID произведения
+    site_id = Column(Integer, ForeignKey(DBMedia.site_id), primary_key=True)  # ID сайта
 
-    media_id = Column(Integer, ForeignKey(DBMedia.media_id))  # ID произведения
-    site_id = Column(Integer, nullable=False)  # ID сайта
-    media_name = Column(String, nullable=False)  # Название произведения
+    update_enabled = Column(Boolean, nullable=False, default=1)
--- bot_utils/db/db_interface.py
+++ bot_utils/db/db_interface.py
@@ -1,8 +1,9 @@
-from sqlalchemy import select
+from sqlalchemy import update, select
+from sqlalchemy.dialects.sqlite import insert
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.util import generic_repr
 
-from db_init import DBMedia
+from db_init import DBMedia, DBUpdates
 import asyncio
 
 
@@ -18,30 +19,73 @@ class DBInterface:
             existing_ids = result.scalars().all()
             return list(existing_ids)
 
-    async def publication_add(self, title_id: int, user_id: int, site_id: int, name: str) -> None:
-        raise NotImplementedError()
+    # FIXME: Возможно изменились входные данные
+    async def publication_add(self, user_id: int, media_id: int, site_id: int, name: str) -> None:
+        stmt_media = insert(DBMedia).values(
+            media_id=media_id, site_id=site_id,
+            media_name=name
+        ).on_conflict_do_nothing()
 
-    async def publication_delete(self, key: int) -> None:
-        raise NotImplementedError()
+        stmt_upd = insert(DBUpdates).values(
+            user_id=user_id, media_id=media_id, site_id=site_id
+        ).on_conflict_do_update(
+            index_elements=[DBUpdates.user_id, DBUpdates.media_id, DBUpdates.site_id],
+            set_=dict(update_enabled=True)
+        )
 
-    async def users_by_publication(self, title_id: int) -> list[tuple[int, int]] | None:
-        raise NotImplementedError()
+        async with self.async_session() as session:
+            await session.execute(stmt_media)
+            await session.commit()
+            await session.execute(stmt_upd)
+            await session.commit()
 
-    async def publications_by_user(self, user_id: int) -> list | None:
-        raise NotImplementedError()
+    # FIXME: Возможно изменились входные данные
+    async def publication_delete(self, user_id: int, media_id: int, site_id: int) -> None:
+        stmt = update(DBUpdates).where(
+            user_id == DBUpdates.user_id, media_id == DBUpdates.media_id, site_id == DBUpdates.site_id
+        ).values(update_enabled=False)
 
-    async def publication_name_by_key(self, key: int) -> str:
-        stmt = select(DBMedia).where(key == DBMedia.media_id)
+        async with self.async_session() as session:
+            await session.execute(stmt)
+            await session.commit()
+
+    # FIXME: Возможно изменились входные данные
+    async def users_by_publication(self, media_id: int, site_id: int) -> list[DBUpdates]:
+        stmt = select(DBUpdates).where(
+            media_id == DBUpdates.media_id, site_id == DBUpdates.site_id,
+            True == DBUpdates.update_enabled
+        )
+        async with self.async_session() as session:
+            result = await session.execute(stmt)
+            return list(result.scalars().all())
+
+    # FIXME: Возможно изменились входные данные
+    async def publications_by_user(self, user_id: int) -> list[DBUpdates]:
+        stmt = select(DBUpdates).where(
+            user_id == DBUpdates.user_id, True == DBUpdates.update_enabled
+        )
+        async with self.async_session() as session:
+            result = await session.execute(stmt)
+            return list(result.scalars().all())
+
+    # FIXME: Возможно изменились входные данные
+    async def publication_get(self, media_id: int, site_id: int) -> DBMedia | None:
+        stmt = select(DBMedia).where(
+            media_id == DBMedia.media_id, site_id == DBMedia.site_id
+        )
         async with self.async_session() as session:
             result = await session.execute(stmt)
             return result.scalars().first()
 
 
-db_new = DBInterface("test_db.sqlite")
+db_new = DBInterface("db.sqlite")
 
 async def main():
-    print(generic_repr(await db_new.check_publication_in_db([1, 2, 3])))
-    print(generic_repr(await db_new.publication_name_by_key(1)))
+    # print(generic_repr(await db_new.publication_add(1158, 1156, 52, "test")))
+    print(generic_repr(await db_new.publication_delete(1158, 1156, 52)))
+    result = await db_new.users_by_publication(1156, 52)
+    print(result)
+    print(len(result))
     print()
 
 
